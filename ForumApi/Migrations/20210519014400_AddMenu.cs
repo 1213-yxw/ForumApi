29d4dde89ffc7d2c0cac10926203bb302b268851
guanlimei <2075901536@qq.com>
2021-05-21 13:52:13 +0800

    submit
    post2

---
+++ ForumApi/Migrations/20210519014400_AddMenu.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ForumApi.Migrations
+{
+    public partial class AddMenu : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Menus",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Url = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Text = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Menus", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Menus",
+                columns: new[] { "Id", "Text", "Url" },
+                values: new object[] { 1, "全部帖子", "/personalCenter/postAll" });
+
+            migrationBuilder.InsertData(
+                table: "Menus",
+                columns: new[] { "Id", "Text", "Url" },
+                values: new object[] { 2, "创建帖子", "/personalCenter/richText" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Menus");
+        }
+    }
+}
